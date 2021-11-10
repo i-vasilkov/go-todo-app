e401e6677e131fb01de3f1fc67e463510efaca87
Ilya Vasilkov <viv.vasilkov@gmail.com>
2021-11-10 21:23:58 +0700

    Added mongo repository implementation

--- internal/domain/todo.go
+++ internal/domain/todo.go
@@ -3,13 +3,16 @@ package domain
 import "time"
 
 type Todo struct {
-	Id        string    `json:"id"`
-	Name      string    `json:"name"`
-	CreatedAt time.Time `json:"created_at"`
+	Id        string    `json:"id" bson:"_id,omitempty"`
+	Name      string    `json:"name" bson:"name,omitempty"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
 }
 
 type UpdateTodoInput struct {
+	Name string `json:"name" binding:"required"`
 }
 
 type CreateTodoInput struct {
+	Name string `json:"name" binding:"required"`
 }
--- internal/repository/mongorep/todo.go
+++ internal/repository/mongorep/todo.go
@@ -3,7 +3,10 @@ package mongorep
 import (
 	"context"
 	"github.com/i-vasilkov/go-todo-app/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"time"
 )
 
 type ToDoRepository struct {
@@ -16,22 +19,93 @@ func NewMongoToDoRepository(db *mongo.Database) *ToDoRepository {
 	}
 }
 
-func (t *ToDoRepository) Get(ctx context.Context, id string) (domain.Todo, error) {
-	return domain.Todo{}, nil
+func (rep *ToDoRepository) Get(ctx context.Context, id string) (domain.Todo, error) {
+	objId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return domain.Todo{}, err
+	}
+
+	var todo domain.Todo
+	err = rep.db.Collection("todos").
+		FindOne(ctx, bson.M{"_id": objId}).
+		Decode(&todo)
+
+	if err != nil {
+		return todo, err
+	}
+
+	return todo, nil
 }
 
-func (t *ToDoRepository) GetAll(ctx context.Context) ([]domain.Todo, error) {
-	return nil, nil
+func (rep *ToDoRepository) GetAll(ctx context.Context) ([]domain.Todo, error) {
+	cursor, err := rep.db.Collection("todos").Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	var todos []domain.Todo
+	if err := cursor.All(ctx, &todos); err != nil {
+		return nil, err
+	}
+
+	return todos, nil
 }
 
-func (t *ToDoRepository) Create(ctx context.Context, in domain.CreateTodoInput) (domain.Todo, error) {
-	return domain.Todo{}, nil
+func (rep *ToDoRepository) Create(ctx context.Context, in domain.CreateTodoInput) (domain.Todo, error) {
+	result, err := rep.db.Collection("todos").
+		InsertOne(ctx, bson.M{
+			"name":       in.Name,
+			"created_at": time.Now().Format(time.RFC3339),
+			"updated_at": time.Now().Format(time.RFC3339),
+		})
+
+	if err != nil {
+		return domain.Todo{}, err
+	}
+
+	objId := result.InsertedID.(primitive.ObjectID)
+
+	var todo domain.Todo
+	err = rep.db.Collection("todos").
+		FindOne(ctx, bson.M{"_id": objId}).
+		Decode(&todo)
+
+	if err != nil {
+		return todo, err
+	}
+
+	return todo, nil
 }
 
-func (t *ToDoRepository) Update(ctx context.Context, id string, in domain.UpdateTodoInput) (domain.Todo, error) {
-	return domain.Todo{}, nil
+func (rep *ToDoRepository) Update(ctx context.Context, id string, in domain.UpdateTodoInput) (domain.Todo, error) {
+	objId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return domain.Todo{}, err
+	}
+
+	update := bson.M{"$set": bson.M{"name": in.Name, "updated_at": time.Now().Format(time.RFC3339)}}
+	_, err = rep.db.Collection("todos").UpdateOne(ctx, bson.M{"_id": objId}, update)
+
+	var todo domain.Todo
+	err = rep.db.Collection("todos").
+		FindOne(ctx, bson.M{"_id": objId}).
+		Decode(&todo)
+
+	if err != nil {
+		return todo, err
+	}
+
+	return todo, nil
+
+	return todo, nil
 }
 
-func (t *ToDoRepository) Delete(ctx context.Context, id string) error {
-	return nil
+func (rep *ToDoRepository) Delete(ctx context.Context, id string) error {
+	objId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	_, err = rep.db.Collection("todos").DeleteOne(ctx, bson.M{"_id": objId})
+	return err
 }
